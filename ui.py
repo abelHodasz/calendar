21240500e41bf5129a78de506b23c82fe6fbfe1f
Abel Hodasz <hodabi@gmail.com>
2019-05-16 13:34:22 +0200

    UI: print_items

--- ui.py
+++ ui.py
@@ -1,22 +1,29 @@
 # printing data, asking for user input
 
 
-def print_menu(items, label):
+def print_menu(label, items):
     print(label)
     for number, item in enumerate(items):
         print(f"\t({number}) {item} ")
 
 
-def get_input(items, label):
+def print_items(label, items="empty"):
+    print(label)
+    if type(items) == list:
+        for item in items:
+            print(f"({item})")
+    elif type(items) == str:
+        print(f"({items})")
+
+
+def get_input(label, items):
     print(label)
     if type(items) == list:
         user_inputs = []
         for item in items:
             user_inputs.append(input(f"{input}: "))
+        return user_inputs
     elif type(items) == str:
         return input(f"{items}: ")
     else:
         raise ValueError("Invalid input call!")
-
-
-
