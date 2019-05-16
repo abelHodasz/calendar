8cd9b2b641b6a2e715511a569dac3c1069180c16
Abel Hodasz <hodabi@gmail.com>
2019-05-16 13:40:52 +0200

    Cal: main structure

--- cal.py
+++ cal.py
@@ -1,1 +1,23 @@
 # main program
+import ui
+import common
+import storage
+
+
+def choose(menu_number, menu):
+    pass
+
+
+def main():
+    menu = ["Quit", "Schedule a new meeting", "Cancel a meeting"]
+    while True:
+        ui.print_items()
+        try:
+            choose(int(ui.get_input("","Menu number:")), menu)
+        except ValueError:
+            ui.print_error("Invalid input, try again!")
+            continue
+                   
+
+if __name__="__main__":
+    main()
